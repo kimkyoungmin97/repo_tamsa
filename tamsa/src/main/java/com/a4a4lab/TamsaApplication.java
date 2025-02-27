afeb13ed0c193a433003f5e39c699de74f8140b3
kimkyoungmin97 <kmboy1210@gmail.com>
2025-02-27 12:05:16 +0900

    2025-02-27

---
+++ tamsa/src/main/java/com/a4a4lab/TamsaApplication.java
@@ -0,0 +1,13 @@
+package com.a4a4lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamsaApplication.class, args);
+	}
+
+}
